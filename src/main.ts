e56081442f0580834b13c3ad0f34ad619dc79832
grze2000 <programista333@gmail.com>
2023-09-30 13:30:57 +0200

    update static file serving config

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', '/src/public'));
-  app.setBaseViewsDir(join(__dirname, '..', '/src/views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
   console.log('Listening on port', process.env.PORT || 3000);
